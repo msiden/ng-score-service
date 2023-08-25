4b1f40111668c543c33d3f01cb601fc4168a5b1e
Martin Sidén <martin.siden@bambora.com>
2023-08-25 09:24:35 +0200

    endpoints wip

--- src/score-service/endpoints.py
+++ src/score-service/endpoints.py
@@ -1,7 +1,13 @@
 
-def get_scores():
-    ...
+def get_scores(size: int = None, level: int = None):
+    return [
+        {'name': 'player1', 'score': 14000, 'level': 0},
+        {'name': 'player2', 'score': 4000, 'level': 1},
+        {'name': 'player3', 'score': 84000, 'level': 0}
+    ]
 
 
-def save_score():
-    ...
+def save_score(score: int, level: int, name: str):
+    return {
+        'position': 30
+    }
