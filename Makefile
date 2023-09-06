cb8fd509d152d5f04b9e5220e9f370202675d5c5
Martin Sidén <martin.siden@bambora.com>
2023-09-06 15:33:31 +0200

    db wip

--- Makefile
+++ Makefile
@@ -4,8 +4,11 @@ build-deploy:
 build-local:
 	docker build --no-cache -t ng-score-service -f ./Dockerfile.dev .
 
+build-test:
+	docker build --no-cache -t ng-score-service-test -f ./Dockerfile.test .
+
 run-local: build-local
 	docker run -ti -p 8081:8081 ng-score-service
 
-# run-local-with-frontend: build-local
-# 	docker-compose up
+run-test: build-test
+	docker run -ti ng-score-service-test bash
--- src/score-service/db.py
+++ src/score-service/db.py
@@ -1,31 +1,66 @@
-from typing import List
-from typing import Optional
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
+from sqlalchemy import String, Integer
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+
+engine = create_engine("sqlite://", echo=True)
+
 
 class Base(DeclarativeBase):
     pass
 
-class User(Base):
-    __tablename__ = "user_account"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[Optional[str]]
-    addresses: Mapped[List["Address"]] = relationship(
-        back_populates="user", cascade="all, delete-orphan"
-    )
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
 
-class Address(Base):
-    __tablename__ = "address"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email_address: Mapped[str]
-    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
-    user: Mapped["User"] = relationship(back_populates="addresses")
+class Scores(Base):
+    __tablename__ = "scores"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    user_name: Mapped[str] = mapped_column(String(30))
+    score: Mapped[int] = mapped_column(Integer)
+    level: Mapped[int] = mapped_column(Integer)
+
     def __repr__(self) -> str:
-        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+        return f"id:{self.id!r}, user_name:{self.user_name!r}, score:{self.score!r}, level:{self.level!r}"
+
+
+# Create database
+def create_all():
+    Base.metadata.create_all(engine)
+
+
+# Write data to DB
+def insert_data():
+    with Session(engine) as session:
+        spongebob = Scores(
+            id="1",
+            user_name="spongebob",
+            score=1000,
+            level=0
+        )
+        sandy = Scores(
+            id="2",
+            user_name="sandy",
+            score=1200,
+            level=0
+        )
+
+        session.add_all([spongebob, sandy])
+        session.commit()
+
+
+def get_data():
+    session = Session(engine)
+
+    stmt = select(Scores).where(Scores.user_name.in_(["spongebob", "sandy"]))
+
+    for user in session.scalars(stmt):
+        print(user)
+
+
+if __name__ == "__main__":
+    create_all()
+    insert_data()
+    get_data()
+
